fcbd0b621bbf914b7c0c29ccdc61f91f32ba3834
aipoh ruth <aipohruth@gmail.com>
2022-12-07 22:26:01 +0100

    new:features

---
+++ src/Data/Data3.jsx
@@ -0,0 +1,28 @@
+import abj from '../Assets/Abuja.jpg';
+import lag from '../Assets/lagos.jpg';
+import cal from '../Assets/calabar.jpg';
+import ph from '../Assets/ph.jpg';
+
+
+export const Location =[
+    {
+        id:1,
+        text:'Abuja',
+        image: abj,
+    },
+    {
+        id:2,
+        text:'Lagos',
+        image: lag,
+    },
+    {
+        id:3,
+        text:'Calabar',
+        image: cal,
+    },
+    {
+        id:4,
+        text:'Port-Harcout',
+        image: ph,
+    },
+]

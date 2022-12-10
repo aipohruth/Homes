e22f679016eba1d60f90325eedd62a439daca840
aipoh ruth <aipohruth@gmail.com>
2022-12-10 15:03:58 +0100

    new features

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -2,31 +2,13 @@ import React, {useState} from 'react';
 import {BsFillBookFill} from 'react-icons/bs';
 import {AiTwotoneStar} from 'react-icons/ai';
 import {FaQuoteLeft} from 'react-icons/fa';
-//import {FiChevronRight,FiChevronLeft} from 'react-icons/fi';
+
 import { testimonials } from '../Data/Data2';
 
 const Testimonials = () => {
-    const[people, setPeople] = useState(testimonials);
-    /*const[index, setIndex] = useState (0);
-
-    useEffect(() =>{
-        const lastIndex = people.length -1;
-        if(index < 0){
-          setIndex(lastIndex)
-        }
-        if(index > lastIndex){
-          setIndex(0)
-        }
-         },[index, people])
-        
-        useEffect(() =>{
-         const slide = setInterval(() =>{
-            setIndex(index + 1)
-          }, 3000)
-          return () => clearInterval(slide)
-        },[index])
-           <button  className='rounded-full p-1 absolute top-[50%] left-4'><FiChevronLeft/></button>
-                <button   className='rounded-full p-1 absolute top-[50%] right-4'><FiChevronRight/></button>*/
+    const[people] = useState(testimonials);
+    
+   
 
 
 
@@ -71,39 +53,3 @@ const Testimonials = () => {
 
 export default Testimonials
 
-
-/* const { id, image, name, title, quote} = person;
-            //more stuff coming here 
-            let position = 'nextSlide'
-            if(personIndex === index){
-              position = 'activeSlide'
-            }
-            if(personIndex === index -1 || (index === 0 && personIndex === people.length -1)){
-              position = 'lastSlide'
-            }*/
-
-
-
-
-            /*
-              <div className=' flex flex-col justify-center items-center'>
-                    <img src={pp3} alt='name' className='w-[80px] h-[80px] rounded-full object-cover my-4' />
-                   
-                    <FaQuoteLeft size={10} />
-                        <p className='text-gray-700 px-10 pt-2 '>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae eligendi, earum 
-                            tempore in sit impedit mane, unde veniam maxime mollitia assumenda
-                              dolores! Vel, quod aperiam.</p>
-                    <span className=' text-sm mt-3 font-bold mb-1 text-gray-700'>Melissa Joe</span>
-                        <div className='flex mb-4 text-[var(--primary-light)]'>
-                <AiTwotoneStar  />
-                <AiTwotoneStar  />
-                <AiTwotoneStar  />
-                <AiTwotoneStar  />
-                <AiTwotoneStar  />
-            </div>
-                </div>
-                <button className='rounded-full p-1 absolute top-[50%] left-4'><FiChevronLeft/></button>
-                <button className='rounded-full p-1 absolute top-[50%] right-4'><FiChevronRight/></button>
-            
-            
-            */
